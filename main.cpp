65905ce4290ba149b872b091eecbe95858c75c5d
Viktor Ivanov <sfaxi19@gmail.com>
2015-11-08 19:03:04 +0300

    create move point

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #define BLUE 2
 using namespace std;
 node hexag[61];
-int visited[61] = { 0 };
+//int visited[61] = { 0 };
 
 void bindNodes(int id, int idHex)
 {
@@ -75,37 +75,89 @@ int searchRad1(int id, int color)
 {
 	int tmp = 0;
 	listNode *tmpList = hexag[id].next;
-	printf("id = %i\nvalue = %i\nnodes: ", hexag[id].id, hexag[id].value);
-
 	while (tmpList != NULL)
 	{
 		if (tmpList->node->value == color)
 			tmp++;
-		printf("%i\t", tmpList->node->value);
 		tmpList = tmpList->next;
 	}
 
 	return tmp;
 }
 
-int move(int who, int were)
+void reColor(int id, int color)
 {
-	listNode *tmpList = hexag[who].next;
-	int rad = 0;
+	listNode *tmpList = hexag[id].next;
 	while (tmpList != NULL)
 	{
-		if (tmpList->node->id == were)
+		if ((tmpList->node->value != color) && (tmpList->node->value!=CLEAN))
 		{
-			rad = 1;
-			continue;
+			tmpList->node->value = color;
 		}
 		tmpList = tmpList->next;
 	}
+}
 
-
-
+bool visited(int id, int visitMass[])
+{
+	for (int i = 0; i < 18; i++)
+	{
+		if (id == visitMass[i]) return 1;
+	}
 	return 0;
 }
+
+int move(int who, int where, int color)
+{
+	int rad = 0;
+	int rad2visited[18] = { 0 };
+	bool repeat = true;
+	listNode *tmpList = hexag[who].next;
+
+	while (tmpList != NULL)
+	{
+		if (!repeat)
+		{
+			listNode *tmpList2 = tmpList->node->next;
+			while (tmpList2 != NULL)
+			{
+				if (!visited(tmpList2->node->id,rad2visited))
+				if (tmpList2->node->id == where)
+				{
+					rad = 2;
+					break;
+				}
+				tmpList2 = tmpList2->next;
+			}
+		}
+		else if (tmpList->node->id == where)
+		{
+			rad = 1;
+			break;
+		}
+
+		tmpList = tmpList->next;
+		if ((tmpList == NULL) && (repeat))
+		{
+			tmpList = hexag[who].next;
+			repeat = false;
+		}
+	}
+
+	if (rad == 1)
+	{
+		hexag[where].value = color;
+		reColor(where,color);
+	}
+	else if (rad == 2)
+	{
+		hexag[who].value = CLEAN;
+		hexag[where].value = color;
+		reColor(where, color);
+	}
+	return 0;
+}
+
 void main()
 {
 	graphCreate();
@@ -122,9 +174,19 @@ void main()
 		printf("\n");
 	}
 
-	hexag[21].value = RED;
+	hexag[6].value = RED;
+	hexag[12].value = RED;
+	hexag[20].value = RED;
+	hexag[22].value = RED;
+	hexag[15].value = BLUE;
+
 	printf("\nSumm = %i\n", searchRad1(13, RED));
-	move(14,13);
+	move(15,13, hexag[15].value);
+
+	for (int i = 0; i < 61; i++)
+	{
+		printf("%i:\t%i\n",i,hexag[i].value);
+	}
 	printf("\nfinish\n");
 	getch();
 }
